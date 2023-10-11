685eef177b8c691fee7d4b224da025be9c16bf21
s22004 <s22004@std.it-college.ac.jp>
2023-10-11 12:00:21 +0900

    12.1.2.3

---
+++ MediaSample/app/src/main/java/jp/ac/it_college/std/s22004/mediasample/MainActivity.kt
@@ -0,0 +1,53 @@
+package jp.ac.it_college.std.s22004.mediasample
+
+import android.media.MediaPlayer
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import jp.ac.it_college.std.s22004.mediasample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var mediaPlayer: MediaPlayer? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mediaPlayer = MediaPlayer()
+        val mediaFileUri = Uri.parse("android.resource://${packageName}/${R.raw.flow}")  //https://~~~~~
+        mediaPlayer?.apply {
+            setDataSource(applicationContext, mediaFileUri)
+            setOnPreparedListener(::mediaPlayerOnPrepared)
+            setOnCompletionListener(::mediaPlayerOnCompletion)
+            prepareAsync()
+        }
+
+        binding.btPlay.setOnClickListener(::onPlayButtonClick)
+    }
+
+    private fun mediaPlayerOnPrepared(mediaPlayer: MediaPlayer) {
+        binding.btPlay.isEnabled = true
+        binding.btBack.isEnabled = true
+        binding.btForward.isEnabled = true
+    }
+
+    private fun mediaPlayerOnCompletion(mediaPlayer: MediaPlayer) {
+        binding.btPlay.setText(R.string.bt_play_play)
+    }
+
+    private fun onPlayButtonClick(view: View) {
+        mediaPlayer?.run {
+            if (isPlaying) {
+                pause()
+                binding.btPlay.setText(R.string.bt_play_play)
+            } else {
+                start()
+                binding.btPlay.setText(R.string.bt_play_pause)
+            }
+        }
+    }
+
+}
