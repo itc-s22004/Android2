865044c94524acb7069c8aac3fa6948b20b991b2
s22004 <s22004@std.it-college.ac.jp>
2023-10-17 11:32:41 +0900

    Media3ServiceSample initial commit

---
+++ Media3ServiceSample/app/src/main/java/jp/ac/it_college/std/s22004/media3servicesample/MainActivity.kt
@@ -0,0 +1,56 @@
+package jp.ac.it_college.std.s22004.media3servicesample
+
+import android.content.ComponentName
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.media3.common.MediaItem
+import androidx.media3.session.MediaController
+import androidx.media3.session.SessionToken
+import com.google.common.util.concurrent.ListenableFuture
+import jp.ac.it_college.std.s22004.media3servicesample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var controllerFuture: ListenableFuture<MediaController>
+
+    private val controller: MediaController?
+        get() = if (controllerFuture.isDone) controllerFuture.get() else null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.play.setOnClickListener { playSample() }
+        binding.pause.setOnClickListener { pauseSmpale() }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        controllerFuture = MediaController.Builder(
+            this,
+            SessionToken(this, ComponentName(this, MediaPlaybackService::class.java))
+        ).buildAsync()
+    }
+
+    override fun onStop() {
+        MediaController.releaseFuture(controllerFuture)
+        super.onStop()
+    }
+
+    private fun playSample() {
+        val controller = this.controller ?: return
+
+        controller.setMediaItem(
+            MediaItem.fromUri("android.resource://${packageName}/${R.raw.miti}")
+        )
+        controller.seekToDefaultPosition(0)
+        controller.play()
+    }
+
+    private fun pauseSmpale() {
+        val controller = this.controller ?: return
+        controller.stop()
+    }
+}
