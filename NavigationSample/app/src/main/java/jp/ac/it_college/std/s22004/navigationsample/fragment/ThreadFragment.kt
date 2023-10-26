595571a43f31e5e961c475771943ddeb5fd8d686
s22004 <s22004@std.it-college.ac.jp>
2023-10-26 09:34:22 +0900

    fragmentを何個か作った

--- NavigationSample/app/src/main/java/jp/ac/it_college/std/s22004/navigationsample/fragment/ThreadFragment.kt
+++ NavigationSample/app/src/main/java/jp/ac/it_college/std/s22004/navigationsample/fragment/ThreadFragment.kt
@@ -1,42 +1,34 @@
 package jp.ac.it_college.std.s22004.navigationsample.fragment
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import jp.ac.it_college.std.s22004.navigationsample.R
-import jp.ac.it_college.std.s22004.navigationsample.databinding.FragmentHomeBinding
+import jp.ac.it_college.std.s22004.navigationsample.databinding.FragmentSecondBinding
+import jp.ac.it_college.std.s22004.navigationsample.databinding.FragmentThreadBinding
 
 
-class HomeFragment : Fragment() {
-
-    private var _binding: FragmentHomeBinding? = null
+class ThreadFragment : Fragment() {
+    private var _binding: FragmentThreadBinding? = null
     private val binding get() = _binding!!
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentThreadBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.homeToNext.setOnClickListener {toNext()}
+        val choise = arguments?.getInt("choise", 0)
+        binding.display.text = "<$choise>"
     }
-
-
 
     override fun onDestroyView() {
-        _binding = null
         super.onDestroyView()
-    }
-    private fun toNext() {
-        findNavController().navigate(R.id.action_homeFragment_to_secondFragment)
     }
 
 }
