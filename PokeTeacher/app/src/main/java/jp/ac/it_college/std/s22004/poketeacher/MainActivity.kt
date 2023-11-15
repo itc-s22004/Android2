46bf611dce80412e9ae6c719b0835282b14d3805
s22004 <s22004@std.it-college.ac.jp>
2023-11-15 10:12:36 +0900

    initial commit

---
+++ PokeTeacher/app/src/main/java/jp/ac/it_college/std/s22004/poketeacher/MainActivity.kt
@@ -0,0 +1,46 @@
+package jp.ac.it_college.std.s22004.poketeacher
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import jp.ac.it_college.std.s22004.poketeacher.ui.theme.PokeTeacherTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PokeTeacherTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PokeTeacherTheme {
+        Greeting("Android")
+    }
+}
